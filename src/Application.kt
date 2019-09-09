10af9750713dc40d38ebdb20be79bdaaee3a27e7
darius-wattimena <dariuswattimena@hotmail.com>
2019-09-09 17:18:08 +0200

    Removed the /json/ prefix in al URL's

--- src/Application.kt
+++ src/Application.kt
@@ -43,211 +43,208 @@ fun Application.module(testing: Boolean = false) {
     routing {
         method(HttpMethod.Get) {
             route("/") {
-                route("json") {
+                /**
+                 * Board
+                 */
 
-                    /**
-                     * Board
-                     */
+                get("/board/{id}") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    call.respondText(
+                        RequestExecuter.execute(GetBoard(request)),
+                        contentType = ContentType.Application.Json
+                    )
+                }
 
-                    get("/board/{id}") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
-                        call.respondText(
-                            RequestExecuter.execute(GetBoard(request)),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
+                get("/board/{id}/detailed") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    call.respondText(
+                        RequestExecuter.execute(GetDetailedBoard(request)),
+                        contentType = ContentType.Application.Json
+                    )
+                }
 
-                    get("/board/{id}/detailed") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
-                        call.respondText(
-                            RequestExecuter.execute(GetDetailedBoard(request)),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
+                get("/board/{id}/statistics") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    call.respondText(
+                        RequestExecuter.execute(GetBoardStatistics(request)),
+                        contentType = ContentType.Application.Json
+                    )
+                }
 
-                    get("/board/{id}/statistics") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
-                        call.respondText(
-                            RequestExecuter.execute(GetBoardStatistics(request)),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
+                get("/board/{id}/getLastAction") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    call.respondText(
+                        RequestExecuter.execute(GetLastBoardAction(request)),
+                        contentType = ContentType.Application.Json
+                    )
+                }
 
-                    get("/board/{id}/getLastAction") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
-                        call.respondText(
-                            RequestExecuter.execute(GetLastBoardAction(request)),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
+                get("/processedBoard/{id}") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    call.respondText(
+                        getBoardsAndProcessActionsOfAllCards(request),
+                        contentType = ContentType.Application.Json
+                    )
+                }
 
-                    get("/processedBoard/{id}") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                route("/testBoard") {
+                    get {
+                        val request = RequestInfo(call.request.headers, "RsU5w4Bn")
                         call.respondText(
                             getBoardsAndProcessActionsOfAllCards(request),
                             contentType = ContentType.Application.Json
                         )
                     }
+                }
 
-                    route("/testBoard") {
-                        get {
-                            val request = RequestInfo(call.request.headers, "RsU5w4Bn")
-                            call.respondText(
-                                getBoardsAndProcessActionsOfAllCards(request),
-                                contentType = ContentType.Application.Json
-                            )
-                        }
-                    }
-
-                    get("/board/{id}/burndownchartinfo") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
-                        val doneListId = call.request.headers["doneListId"]
-                        val startDate = call.request.headers["startDate"]
-                        val endDate = call.request.headers["endDate"]
-                        val today = call.request.headers["today"]
-                        call.respondText(
-                            RequestExecuter.execute(
-                                GetBurndownChartInfo(request,
-                                    doneListId.toString(),
-                                    startDate.toString(),
-                                    endDate.toString(),
-                                    today.toString()
-                                )
-                            ),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
-
-                    get("/board/{id}/todayburndownchartinfo") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
-                        val doneListId = call.request.headers["doneListId"]
-                        val today = call.request.headers["today"]
-                        call.respondText(
-                            RequestExecuter.execute(
-                                GetTodayBurndownChartInfo(request,
-                                    doneListId.toString(),
-                                    today.toString()
-                                )
-                            ),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
-
-                    get("/board/{id}/sync/leaderboard") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
-                        val doneListId = call.request.headers["doneListId"]
-                        val doingListId = call.request.headers["doingListId"]
-                        val testingListId = call.request.headers["testingListId"]
-                        val reviewingListId = call.request.headers["reviewingListId"]
-                        val startDate = call.request.headers["startDate"]
-                        val endDate = call.request.headers["endDate"]
-                        call.respondText(
-                            RequestExecuter.execute(SyncBoardLeaderboardData(
-                                request,
+                get("/board/{id}/burndownchartinfo") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    val doneListId = call.request.headers["doneListId"]
+                    val startDate = call.request.headers["startDate"]
+                    val endDate = call.request.headers["endDate"]
+                    val today = call.request.headers["today"]
+                    call.respondText(
+                        RequestExecuter.execute(
+                            GetBurndownChartInfo(request,
                                 doneListId.toString(),
-                                doingListId.toString(),
-                                testingListId.toString(),
-                                reviewingListId.toString(),
+                                startDate.toString(),
+                                endDate.toString(),
+                                today.toString()
+                            )
+                        ),
+                        contentType = ContentType.Application.Json
+                    )
+                }
+
+                get("/board/{id}/todayburndownchartinfo") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    val doneListId = call.request.headers["doneListId"]
+                    val today = call.request.headers["today"]
+                    call.respondText(
+                        RequestExecuter.execute(
+                            GetTodayBurndownChartInfo(request,
+                                doneListId.toString(),
+                                today.toString()
+                            )
+                        ),
+                        contentType = ContentType.Application.Json
+                    )
+                }
+
+                get("/board/{id}/sync/leaderboard") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    val doneListId = call.request.headers["doneListId"]
+                    val doingListId = call.request.headers["doingListId"]
+                    val testingListId = call.request.headers["testingListId"]
+                    val reviewingListId = call.request.headers["reviewingListId"]
+                    val startDate = call.request.headers["startDate"]
+                    val endDate = call.request.headers["endDate"]
+                    call.respondText(
+                        RequestExecuter.execute(SyncBoardLeaderboardData(
+                            request,
+                            doneListId.toString(),
+                            doingListId.toString(),
+                            testingListId.toString(),
+                            reviewingListId.toString(),
+                            startDate.toString(),
+                            endDate.toString()
+                        )),
+                        contentType = ContentType.Application.Json
+                    )
+                }
+
+                get("/board/{id}/leaderboard") {
+                    val startDate = call.request.headers["startDate"]
+                    val endDate = call.request.headers["endDate"]
+                    call.respondText(
+                        RequestExecuter.execute(
+                            GetLeaderboardData(call.parameters["id"]!!,
                                 startDate.toString(),
                                 endDate.toString()
-                            )),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
+                            )
+                        ),
+                        contentType = ContentType.Application.Json
+                    )
+                }
 
-                    get("/board/{id}/leaderboard") {
-                        val startDate = call.request.headers["startDate"]
-                        val endDate = call.request.headers["endDate"]
-                        call.respondText(
-                            RequestExecuter.execute(
-                                GetLeaderboardData(call.parameters["id"]!!,
-                                    startDate.toString(),
-                                    endDate.toString()
-                                )
-                            ),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
+                /**
+                 * List
+                 */
 
-                    /**
-                     * List
-                     */
+                get("/list/{id}") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    call.respondText(
+                        RequestExecuter.execute(GetList(request)),
+                        contentType = ContentType.Application.Json
+                    )
+                }
 
-                    get("/list/{id}") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
-                        call.respondText(
-                            RequestExecuter.execute(GetList(request)),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
+                get("/list/{id}/detailed") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    call.respondText(
+                        RequestExecuter.execute(GetDetailedList(request)),
+                        contentType = ContentType.Application.Json
+                    )
+                }
 
-                    get("/list/{id}/detailed") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
-                        call.respondText(
-                            RequestExecuter.execute(GetDetailedList(request)),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
+                /**
+                 * Card
+                 */
 
-                    /**
-                     * Card
-                     */
+                get("/card/{id}") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    call.respondText(
+                        RequestExecuter.execute(GetCard(request)),
+                        contentType = ContentType.Application.Json
+                    )
+                }
 
-                    get("/card/{id}") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
-                        call.respondText(
-                            RequestExecuter.execute(GetCard(request)),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
+                get("/card/{id}/actions") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    call.respondText(
+                        RequestExecuter.execute(GetCardActions(request)),
+                        contentType = ContentType.Application.Json
+                    )
+                }
 
-                    get("/card/{id}/actions") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
-                        call.respondText(
-                            RequestExecuter.execute(GetCardActions(request)),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
+                /**
+                 * Action
+                 */
 
-                    /**
-                     * Action
-                     */
+                get("/action/{id}") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    call.respondText(
+                        RequestExecuter.execute(GetAction(request)),
+                        contentType = ContentType.Application.Json
+                    )
+                }
 
-                    get("/action/{id}") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
-                        call.respondText(
-                            RequestExecuter.execute(GetAction(request)),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
+                /**
+                 * Member
+                 */
 
-                    /**
-                     * Member
-                     */
+                get("member/{id}") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    call.respondText(
+                        RequestExecuter.execute(GetMember(request)),
+                        contentType = ContentType.Application.Json
+                    )
+                }
 
-                    get("member/{id}") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
-                        call.respondText(
-                            RequestExecuter.execute(GetMember(request)),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
+                get("board/{id}/members") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    call.respondText(
+                        RequestExecuter.execute(GetBoardMembers(request)),
+                        contentType = ContentType.Application.Json
+                    )
+                }
 
-                    get("board/{id}/members") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
-                        call.respondText(
-                            RequestExecuter.execute(GetBoardMembers(request)),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
-
-                    get("board/{id}/sync/members") {
-                        val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
-                        call.respondText(
-                            RequestExecuter.execute(SyncMembers(request)),
-                            contentType = ContentType.Application.Json
-                        )
-                    }
+                get("board/{id}/sync/members") {
+                    val request = RequestInfo(call.request.headers, call.parameters["id"]!!)
+                    call.respondText(
+                        RequestExecuter.execute(SyncMembers(request)),
+                        contentType = ContentType.Application.Json
+                    )
                 }
             }
         }
